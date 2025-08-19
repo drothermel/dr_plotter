94274c49b2d54744e89ab56302af79bf73afb18e
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-08-19 15:46:04 -0400

    create utils directory and rename utils/scripting.py

---
+++ src/dr_plotter/utils/__init__.py
@@ -0,0 +1,11 @@
+"""
+Utility functions for dr_plotter.
+"""
+
+from .scripting import setup_arg_parser, show_or_save_plot, create_and_render_plot
+
+__all__ = [
+    "setup_arg_parser",
+    "show_or_save_plot", 
+    "create_and_render_plot"
+]
