d225daf333082a7ca69e6ea61e863186ea99be86
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-08-22 19:07:39 -0400

    update scatter to use style applicator

--- src/dr_plotter/plotters/scatter.py
+++ src/dr_plotter/plotters/scatter.py
@@ -2,9 +2,12 @@
 Atomic plotter for scatter plots with multi-series support.
 """
 
-from typing import Dict, List
+from typing import Any, Dict, List, Set
+
+import pandas as pd
 
 from dr_plotter import consts
+from dr_plotter.legend import Legend
 from dr_plotter.theme import SCATTER_THEME, Theme
 from dr_plotter.types import BasePlotterParamName, SubPlotterParamName, VisualChannel
 
@@ -12,21 +15,12 @@ from .base import BasePlotter
 
 
 class ScatterPlotter(BasePlotter):
-    """
-    An atomic plotter for creating scatter plots with multi-series support.
-    """
-
-    # Declarative configuration
     plotter_name: str = "scatter"
     plotter_params: List[str] = []
     param_mapping: Dict[BasePlotterParamName, SubPlotterParamName] = {}
-    enabled_channels: Dict[VisualChannel, bool] = {
-        "hue": True,
-        "size": True,
-        "marker": True,
-        "alpha": True,
-    }
+    enabled_channels: Set[VisualChannel] = {"hue", "size", "marker", "alpha"}
     default_theme: Theme = SCATTER_THEME
+    use_style_applicator: bool = True
 
-    def _draw(self, ax, data, legend, **kwargs):
+    def _draw(self, ax: Any, data: pd.DataFrame, legend: Legend, **kwargs: Any) -> None:
         ax.scatter(data[consts.X_COL_NAME], data[consts.Y_COL_NAME], **kwargs)
