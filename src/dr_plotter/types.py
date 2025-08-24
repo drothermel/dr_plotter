49796f98ae166211aa7fb7e996c0d00547c9dc12
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-08-24 16:40:19 -0400

    remove dead code from types.py

--- src/dr_plotter/types.py
+++ src/dr_plotter/types.py
@@ -1,5 +1,9 @@
+from typing import Dict, Set
+
 type BasePlotterParamName = str
 type SubPlotterParamName = str
 type VisualChannel = str
 type ColName = str
 type StyleAttrName = str
+type Phase = str
+type ComponentSchema = Dict[str, Set[str]]
