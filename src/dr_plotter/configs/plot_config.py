7f4e8945b951ed69899699a34f9e52dda39a34ca
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-09-02 18:08:03 -0400

    update plot_config to instantiate Layout/Style/Legend cfgs from_input

--- src/dr_plotter/configs/plot_config.py
+++ src/dr_plotter/configs/plot_config.py
@@ -6,7 +6,6 @@ from typing import Any
 from dr_plotter.configs.layout_config import LayoutConfig
 from dr_plotter.configs.legend_config import LegendConfig
 from dr_plotter.configs.style_config import StyleConfig
-from dr_plotter.legend_manager import resolve_legend_config
 from dr_plotter.plot_presets import PLOT_CONFIGS
 
 
@@ -18,9 +17,9 @@ class PlotConfig:
 
     def __post_init__(self) -> None:
         self.validate()
-        self.layout = self._resolve_layout_config()
-        self.style = self._resolve_style_config()
-        self.legend = self._resolve_legend_config()
+        self.layout = LayoutConfig.from_input(self.layout)
+        self.style = StyleConfig.from_input(self.style)
+        self.legend = LegendConfig.from_input(self.legend)
 
     def validate(self) -> None:
         pass
@@ -34,59 +33,3 @@ class PlotConfig:
         preset_config = PLOT_CONFIGS[preset_name]
         return cls(**preset_config)
 
-    def _resolve_layout_config(self) -> LayoutConfig:
-        if self.layout is None:
-            return LayoutConfig()
-        elif isinstance(self.layout, LayoutConfig):
-            return self.layout
-        elif isinstance(self.layout, tuple):
-            assert len(self.layout) in {2, 3}
-            kwarg_index = 2
-            has_kwargs = len(self.layout) == kwarg_index + 1
-            if has_kwargs:
-                return LayoutConfig(
-                    rows=self.layout[0], cols=self.layout[1], **self.layout[2]
-                )
-            return LayoutConfig(rows=self.layout[0], cols=self.layout[1])
-        elif isinstance(self.layout, dict):
-            return LayoutConfig(**self.layout)
-
-        return LayoutConfig()
-
-    def _resolve_style_config(self) -> StyleConfig:
-        if self.style is None:
-            return StyleConfig()
-        elif isinstance(self.style, StyleConfig):
-            return self.style
-        elif isinstance(self.style, str):
-            return StyleConfig(theme=self.style)
-        elif isinstance(self.style, dict):
-            return StyleConfig(**self.style)
-
-        return StyleConfig()
-
-    def _resolve_legend_config(self) -> LegendConfig:
-        if self.legend is None:
-            return LegendConfig()
-
-        if isinstance(self.legend, LegendConfig):
-            return self.legend
-
-        if isinstance(self.legend, str):
-            return resolve_legend_config(self.legend)
-
-        assert isinstance(self.legend, dict), (
-            f"Legend must be string, dict, or LegendConfig, got {type(self.legend)}"
-        )
-        return self._convert_legend_dict_to_config(self.legend)
-
-    def _convert_legend_dict_to_config(
-        self, legend_dict: dict[str, Any]
-    ) -> LegendConfig:
-        legend_kwargs = {}
-        for key, value in legend_dict.items():
-            if key == "style":
-                legend_kwargs["strategy"] = value
-            else:
-                legend_kwargs[key] = value
-        return LegendConfig(**legend_kwargs)
